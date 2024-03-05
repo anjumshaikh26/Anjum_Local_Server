210f7a1360fa6cceeee82db119edd01b02a6eb41
anjum <anjum.shaikh2695@gmail.com>
2024-03-05 22:26:11 +0530

    arraylis demo4

---
+++ Git_Demo/src/com/velocity/ArrayListDemo4.java
@@ -0,0 +1,11 @@
+package com.velocity;
+
+public class ArrayListDemo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Arraylist demo4");
+
+	}
+
+}
